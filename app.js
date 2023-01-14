71f647705b5538c452f5a63b257642cc1e775c18
Yevhen <mr.undesired@gmail.com>
2023-01-14 20:50:24 +0200

    add JS tasks

--- app.js
+++ app.js
@@ -12,13 +12,52 @@ for (let i = 0; i < addToCartBtns.length; i++) {
 
 
 let likeBtn = document.querySelectorAll('.like-img');
+
+
 likeBtn.forEach((btn) => btn.addEventListener('click', function(e){
-    // if (btn.classList.contains('liked')) {
-    //     btn.classList.remove('liked');
+    // if (btn.classList.contains('liked')){
+    //     btn.classList.remove('liked')
     // } else {
-    //     btn.classList.add('liked');
+    //     btn.classList.add('liked')
     // }
-    // btn.classList.toggle('liked');
+    // console.log(e.target);
+    e.target.classList.toggle('liked');
+    e.target.style.transition = 'all 200ms ease-out';
 }))
 
 
+let btnDetails = document.querySelectorAll('.new-form');
+let modal = document.querySelector('.modal');
+let closeBtn = document.querySelector('.btn-close');
+let sendBtn = document.querySelector('.button-on-form');
+sendBtn.addEventListener('click', function(e){
+    e.preventDefault();
+})
+
+btnDetails.forEach((item) => item.addEventListener('click', openModal));
+closeBtn.addEventListener('click', closeModal)
+
+function openModal(){
+    modal.classList.add('show');
+}
+
+function closeModal(){
+    modal.classList.remove('show');
+}
+
+
+modal.addEventListener('click', function(e){
+    if (e.target === modal || e.target === sendBtn) {
+        closeModal()
+    }
+})
+
+
+function scrolling() {
+    if (document.documentElement.scrollTop > document.documentElement.scrollHeight/2) {
+        openModal()
+    }
+}
+
+
+window.addEventListener('scroll' , scrolling);
